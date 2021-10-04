117333f239d1211e9eda7aed46cbd9ee854e9b6d
MANSI MISTRY <65220621+MansiLad@users.noreply.github.com>
2021-10-04 19:11:58 +0530

    Update create-cluster.sh

--- create-cluster.sh
+++ create-cluster.sh
@@ -15,13 +15,6 @@ echo "AKS CLI installed"
 echo "Step 1 - Creating AKS cluster $clusterName"
 # Use either one of the "az aks create" commands below
 # For users working in their personal Azure account
-# This commmand will not work for the Cloud Lab users, because you are not allowed to create Log Analytics workspace for monitoring
-az aks create \
---resource-group $resourceGroup \
---name $clusterName \
---node-count 1 \
---enable-addons monitoring \
---generate-ssh-keys
 
 # For Cloud Lab users
 az aks create \
@@ -34,7 +27,7 @@ az aks create \
 # This command will is a substitute for "--enable-addons monitoring" option in the "az aks create"
 # Use the log analytics workspace - Resource ID
 # For Cloud Lab users, go to the existing Log Analytics workspace --> Properties --> Resource ID. Copy it and use in the command below.
-az aks enable-addons -a monitoring -n $clusterName -g $resourceGroup --workspace-resource-id "/subscriptions/6c39f60b-2bb1-4e37-ad64-faaf30beaca4/resourcegroups/cloud-demo-153430/providers/microsoft.operationalinsights/workspaces/loganalytics-153430"
+az aks enable-addons -a monitoring -n $clusterName -g $resourceGroup --workspace-resource-id "/subscriptions/39eb809d-d780-48c8-af78-b90b5eb28823/resourcegroups/cloud-demo-153430/providers/microsoft.operationalinsights/workspaces/loganalytics-153430"
 
 echo "AKS cluster created: $clusterName"
 
